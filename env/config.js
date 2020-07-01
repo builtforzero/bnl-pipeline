b2dcb90e5f0a39715440831280ece99a5499c158
koffeeya <kavya.beheraj@gmail.com>
2020-07-01 11:16:26 -0400

    added .gitignore

---
+++ env/config.js
@@ -0,0 +1,7 @@
+var config = {
+    MY_KEY : '123456',
+    SECRET_KEY : '56789',
+    KEY_2 : '101010'
+}
+
+export { config };
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { config } from "/env/config.js"
+
+var mykey = config.MY_KEY;
+
+console.log(config.KEY_2)
