34d323b0c60d67b7f68f7fb97f566a9b51b25e80
koffeeya <kavya.beheraj@gmail.com>
2020-07-08 15:29:21 -0400

    working on file uploader

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,68 @@
-/* How to run this file
-    1. Navigate to the 'scripts' folder
-    2. In the terminal, run 'watchify main.js -o bundle.js -v'
-    3. Click on live server in VSCode
-*/
+let state = {
+    data: null,
+    csv: null,
+}
+
+import {
+    keys
+} from "../env/config.js";
+
+const bucketName = keys.BUCKET_NAME
+const bucketRegion = keys.BUCKET_REGION
+const identityPoolId = keys.IDENTITY_POOL_ID
+
+AWS.config.region = bucketRegion; 
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: identityPoolId});
+
+const s3 = new AWS.S3({
+    apiVersion: '2016-06-10',
+    params: {
+        Bucket: bucketName
+    }
+});
+
+function uploadToAWS(file, docTitle, bucketName) {
+    console.log("uploading to AWS")
+    
+    // Read content from the file
+    /* const fileContent = fs.readFileSync(file); */
+
+    // Set up S3 upload parameters
+    const params = {
+        Bucket: bucketName,
+        Key: docTitle, // File name you want to save as in S3
+        Body: file,
+    };
+
+    // Upload files to the bucket
+    s3.upload(params, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+    });
+}
 
 
-var config = require('../env/config.js')
-var AWS = require("aws-sdk")
+const inputElement = document.getElementById("fileUpload");
+inputElement.addEventListener("change", handleFile, false);
 
-var keyId = config.config.AWS_KEY_ID
-var accessKey = config.config.AWS_ACCESS_KEY
 
-console.log("Why?")
+function handleFile() {
+    console.log("handling file")
+
+    const fileList = this.files;
+
+    Papa.parse(fileList[0], {
+        dynamicTyping: true,
+        header: true,
+        complete: function(results) {
+            state.data = results.data;
+            state.csv = Papa.unparse(results.data);
+            console.log(state);
+        }
+    });
+
+    uploadToAWS(state.csv, "testFile.csv", bucketName);
+
+}
