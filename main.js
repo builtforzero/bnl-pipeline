4c23e7221a5078ce1b0f4666704ded982f44190a
koffeeya <kavya.beheraj@gmail.com>
2020-07-10 14:42:33 -0400

    added env file

--- main.js
+++ main.js
@@ -1,86 +1,23 @@
-const random = Math.floor(Math.random() * Math.floor(1000))
+const d3 = require('d3');
+const express = require('express');
+const Papa = require('papaparse');
+const AWS = require('aws-sdk')
 
-let state = {
-    data: null,
-    csv: null,
-    fileList: null,
-    title: "test" + random.toString() + ".csv"
-}
+const {
+    JSDOM
+} = require("jsdom");
+const {
+    window
+} = new JSDOM("");
+const $ = require("jquery")(window);
 
-const formatTime = d3.timeFormat("%X");
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'))
+app.use(express.static('public'));
 
-function updateStatus(newStatus) {
-    d3.select(".status")
-        .append("div")
-        .html(`${newStatus} <b style='color:gray;'> | ${formatTime(Date.now())} </b>` + "<br><br>")
-}
+require('dotenv').config();
+console.log(process.env);
 
-updateStatus(`'${state.title}' set as random file name`)
-
-import {
-    keys
-} from "./config.js";
-
-const bucketName = keys.BUCKET_NAME
-const bucketRegion = keys.BUCKET_REGION
-const identityPoolId = keys.IDENTITY_POOL_ID
-
-AWS.config.region = bucketRegion;
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: identityPoolId,
-});
-
-const s3 = new AWS.S3({
-    apiVersion: '2006-03-01',
-    params: {
-        Bucket: bucketName
-    }
-});
-
-
-
-function uploadToAWS(file, docTitle, bucketName) {
-    updateStatus(`uploading ${state.title} to AWS`)
-
-    // Set up S3 upload parameters
-    const params = {
-        Bucket: bucketName,
-        Key: docTitle, // File name you want to save as in S3
-        Body: file,
-    };
-
-    // Upload files to the bucket
-    s3.upload(params, function (err, data) {
-        if (err) {
-            throw err,
-                updateStatus(`${state.title} was not uploaded. ${err}`)
-        }
-        updateStatus(`${state.title} uploaded successfully to ${data.Location}`);
-    });
-}
-
-
-const inputElement = document.getElementById("fileUpload");
-inputElement.addEventListener("change", viewFile, false);
-
-function viewFile() {
-    updateStatus(`File ${state.title} chosen`)
-    state.fileList = this.files;
-
-    Papa.parse(state.fileList[0], {
-        dynamicTyping: true,
-        header: true,
-        complete: function (results) {
-            state.data = results.data;
-            state.csv = Papa.unparse(results.data);
-            updateStatus(`${state.title} parsed`)
-        }
-    });
-}
-
-const uploadElement = d3
-    .select("#fileSubmit")
-    .on("click", function () {
-        updateStatus(`${state.title} submitting`)
-        uploadToAWS(state.csv, state.title, bucketName);
-    });
+const bucketName = process.env.BUCKET_NAME;
+const bucketRegion = process.env.BUCKET_REGION;
+const identityPoolId = process.env.IDENTITY_POOL_ID;
