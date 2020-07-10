f74f74068e605548cea75172aac7c88d01dcdb9a
koffeeya <kavya.beheraj@gmail.com>
2020-07-10 10:22:34 -0400

    testing config file

---
+++ config.js
@@ -0,0 +1,9 @@
+const keys = {
+    BUCKET_NAME: 'bnl-pipeline',
+    BUCKET_REGION: 'us-east-1',
+    IDENTITY_POOL_ID: "us-east-1:d0a7ba49-1f98-4198-8dbf-ef06f96e3ec2",
+}
+
+export {
+    keys
+};
