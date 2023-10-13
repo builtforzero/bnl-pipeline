74c8cdd18655dda33fb0249610f1e23e776e4de0
esxtang <esxtang@gmail.com>
2023-10-13 11:02:38 -0700

    updated to use subpopname field in subpopulation table

--- client/js/supabase.js
+++ client/js/supabase.js
@@ -3,17 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabase = createClient(process.env.supabaseUrl, process.env.supabaseAnonKey);
 
 export async function insertData(formData) {
-    // Prepare data structure - one row per population
-    let cleanData = [
-        {'populationTag': '[All]', 'popSubpopDemo': 'All-All-All'},
-        {'populationTag': '[All Singles]', 'popSubpopDemo': 'Single Adults-All-All'},
-        {'populationTag': '[Veteran]', 'popSubpopDemo': 'Single Adults-Veteran-All'},
-        {'populationTag': '[Chronic]', 'popSubpopDemo': 'Single Adults-Chronic-All'},
-        {'populationTag': '[Chronic Veteran]', 'popSubpopDemo': 'Single Adults-Chronic Veteran-All'},
-        {'populationTag': '[Youth]', 'popSubpopDemo': 'Youth-All-All'},
-        {'populationTag': '[Families]', 'popSubpopDemo': 'Families-All-All'}
-    ];
-
     // Translation from form field names to Supabase column names
     const cleanFieldNames = {
         'ACTIVELY HOMELESS NUMBER': 'actively_homeless',
@@ -27,10 +16,36 @@ export async function insertData(formData) {
         // 'ACTIVELY HOMELESS INDIVIDUALS IN FAMILIES': 'actively_homeless_in_families'
     };
 
+    // List of subpopulations being accepted
+    const acceptedSubpopulations = ['All', 'All Singles', 'Veteran', 'Chronic', 'Chronic Veteran', 'Youth', 'Families'];
+
+    // Get subpopulation records from Supabase
+    let { data:subpopulationRecords, error:subpoplationError } = await supabase
+        .from('subpopulations')
+        .select('*')
+        .eq('accountname', formData['Community'])
+        .in('subpopname', acceptedSubpopulations);
+
+    // If there are no subpopulation records, throw an error
+    if(subpoplationError) {
+        return 'ERROR: ' + subpoplationError.message;
+    } else if(subpopulationRecords.length <= 0) {
+        return 'ERROR: No subpopulation records were found for this community.';
+    }
+
+    // If subpopulation records are found, proceed to submit the data
+    // Add subpopulation nicknames and ids to clean data array
+    let cleanData = subpopulationRecords.map((record) => {
+        return {
+            'subpopname': record.subpopname,
+            'subpopulation_id': record.id
+        };
+    });
+
     // Restructure data to one row per population with corrected column names
     for(let field in formData) {
         for(let i = 0; i < cleanData.length; i++) {
-            if(field.substring(field.indexOf('['), field.lastIndexOf(']') + 1) == cleanData[i].populationTag) {
+            if(field.substring(field.indexOf('[') + 1, field.lastIndexOf(']')) == cleanData[i].subpopname) {
                 let fieldName = field.indexOf("]") >= 0 ? field.split("] ")[1] : field;
                 if(cleanFieldNames[fieldName]) {
                     cleanData[i][cleanFieldNames[fieldName]] = formData[field];
@@ -45,33 +60,21 @@ export async function insertData(formData) {
     let monthNum = monthNames.indexOf(formData.Month.split(' ')[0]) + 1;
     let formattedMonth = formData.Month.split(' ')[1].toString() + '-' + (monthNum > 9 ? monthNum.toString() : '0' + monthNum.toString()) + '-01';
 
-    // Get subpopulation ids from Supabase
-    let { data:subpopulationData, error:subpoplationError } = await supabase
-        .from('subpopulations')
-        .select('*')
-        .eq('accountname', formData['Community']);
-    if(subpoplationError) {
-        return 'ERROR: ' + subpoplationError.message;
-    } else if(subpopulationData.length <= 0) {
-        return 'ERROR: No subpopulation records were found for this community.';
-    } else {
-        // Add subpopulation id, month, name, email, and org + remove helper fields from each row
-        for(let i = 0; i < cleanData.length; i++) {
-            cleanData[i].subpopulation_id = subpopulationData.find((record) => record.accountname + record.population + '-' + record.subpopulation + '-' + record.demographic == formData['Community'] + cleanData[i].popSubpopDemo).id;
-            cleanData[i].date_interval_start = formattedMonth;
-            cleanData[i].name = formData['Name'];
-            cleanData[i].email = formData['Email Address'];
-            cleanData[i].organization = formData['Organization'];
-            delete cleanData[i].populationTag;
-            delete cleanData[i].popSubpopDemo;
-        }
-        // Insert data into Supabase
-        let { data, error } = await supabase
-            .from('community_reported_population')
-            .insert(cleanData);
-        if(error) {
-            return 'ERROR: ' + error.message;
-        }
-        return 'SUCCESS';
+    // Add month, name, email, and org + remove subpopname helper field from each row
+    for(let i = 0; i < cleanData.length; i++) {
+        cleanData[i].date_interval_start = formattedMonth;
+        cleanData[i].name = formData['Name'];
+        cleanData[i].email = formData['Email Address'];
+        cleanData[i].organization = formData['Organization'];
+        delete cleanData[i].subpopname;
     }
+    
+    // Insert data into Supabase
+    let { data, error } = await supabase
+        .from('community_reported_population')
+        .insert(cleanData);
+    if(error) {
+        return 'ERROR: ' + error.message;
+    }
+    return 'SUCCESS';
 }
